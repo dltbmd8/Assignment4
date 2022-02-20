ad3f4afb90121dacdb735a114a0721b8e0d4ebc8
dltbmd8 <96565727+dltbmd8@users.noreply.github.com>
2022-02-21 00:51:07 +0600

    Add files via upload

---
+++ vegan/js/js.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".Veggie").mouseover(function(){
+      $(this).css("color", "black");
+    });
+    $(".Veggie").mouseout(function(){
+      $(this).css("color", "#588c7e");
+    });
+  });
+  $(document).ready(function(){
+    $(".Baby").mouseover(function(){
+      $(this).css("color", "black");
+    });
+    $(".Baby").mouseout(function(){
+      $(this
+      ).css("color", "#588c7e");
+    });
+  });
